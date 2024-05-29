9e7dbd4208f5d9f1c82ce4c7ba3f19cd02257188
Martinez, Alejandra <le.alejandria@gmail.com>
2024-05-29 14:27:50 +0100

    Add newAppointment controller saving to db

--- Models/Appointment.cs
+++ Models/Appointment.cs
@@ -8,9 +8,17 @@ namespace Appointments.Models
     public class Appointment
     {
         public int appointment_id { get; set; }
+
+        public DateOnly appointment_date { get; set; }
         public TimeSpan appointment_time { get; set; }
         public bool is_new { get; set; }
         public string style_name { get; set; }
         public string emp_name { get; set; }
+
+        public string customer { get; set;}
+        public string phone { get; set;}
+        public int employee_id { get; set; }
+        public int style_id { get; set; }
+    
     }
 }
